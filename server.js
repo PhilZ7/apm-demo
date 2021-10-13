cfa161b8bf50f877b1d7f52d49cf3e600a5c0905
PhilZ7 <phil84_@hotmail.com>
2021-10-12 23:12:25 -0500

    fixing server

--- server.js
+++ server.js
@@ -7,45 +7,41 @@ const rollbar = new Rollbar({
     captureUnhandledRejections: true
 })
 
-app.use(express.json())
-
-app.use('/style', express.static(path.join(__dirname, './public/styles.css')))
-
 const app = express()
 
+app.use(express.json())
 
-app.get('/', (req, res) => {
+app.use('/style', express.static(path.join(__dirname,'./public/styles.css')))
+
+app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
     rollbar.info('html file served successfully')
 })
 
 let students = []
 
-app.post('/api/student', (req, res) => {
+app.post('/api/student', (req,res) => {
     let { name } = req.body
     name = name.trim()
 
-    const index = students.findIndex(studentName=> studentName === name)
+    const index = students.findIndex(studentName => studentName = name)
 
-    if(index === -1 && name !== ''){
+    if(index === -1 && name != ''){
         students.push(name)
-        rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+        rollbar.log('Student added successfully', {author:'Dj',type:'manual entry'})
         res.status(200).send(students)
     } else if (name === ''){
         rollbar.error('No name given')
-        res.status(400).send('must provide a name.')
+        res.status(400).send('please provide a name.')
     } else {
         rollbar.error('student already exists')
-        res.status(400).send('that student already exists')
+        res.status(400).send('this student already exists')
     }
-
 })
 
 app.use(rollbar.errorHandler())
 
 const port = process.env.PORT || 4400
 
-app.listen(port, () => console.log(`server up on ${port}.`))
 
-
-
+app.listen(port, () => console.log(`server up and running on ${port}!`))
